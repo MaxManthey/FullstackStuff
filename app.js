4fc43ef23738d80de5dcdd44878edbe223d3a4ae
Max Manthey <max@Maxs-MacBook-Pro.local>
2021-02-07 18:48:13 +0000

    added demo page

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-
 	res.send('Express app running');
-
 });
+
+app.get('/demo', (req, res) => {
+	res.set('X-full-stack', '4life');
+	res.status(418);
+	res.send('Demo page');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
